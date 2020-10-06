bcf8e0f8c211feff51c46c07ace5b6d3d5c6efd0
Anwesh43 <anweshthecool0@gmail.com>
2020-10-07 01:41:46 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcircleexpandlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcircleexpandlineview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedcircleexpandlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.circleexpandlineview.CircleExpandLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleExpandLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
